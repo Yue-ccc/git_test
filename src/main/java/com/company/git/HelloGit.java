c5506b77f09005c09d53e8851b118a415314b610
Yue <Yue@qq.com>
2022-11-23 22:24:52 +0800

    master commit

--- src/main/java/com/company/git/HelloGit.java
+++ src/main/java/com/company/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("git test");
         System.out.println("git test");
         System.out.println("git test");
+        System.out.println("master");
     }
 }
